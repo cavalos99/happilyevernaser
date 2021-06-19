228e2658a29c1ea3beb077569a8e0e71d5ec5867
Christopher Avalos <ca44872@gmail.com>
2021-06-19 13:44:50 -0700

    Wedding Party
    Add wedding party section (unfinished)

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,3 +1,4 @@
+document.cookie = "witcher=Geralt; SameSite=None; Secure"
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -18,3 +19,32 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+//
+// $(".weddingPartyButton").focusin(function(){
+//     $('.weddingPartyButton').css('color',''); // Remove Before set color to selected button
+//     $(this).css("color", "red"); // Set Color to Selected Button
+// });
+
+function myFunction(elem) {
+  // var x = document.getElementById("bridesmaids");
+  // var description = elem.getAttribute('bridesmaids');
+  // x.innerHTML = description;
+
+  var button = document.getElementsByClassName('weddingPartyButton');
+
+  for (var i = 0; i < button.length; i++) {
+    button[i].classList.remove('active-weddingPartyButton');
+  }
+
+  elem.classList.add('active-weddingPartyButton');
+}
+
+$('body').on('click', '[data-toggle="tab"]', function(e) {
+  e.preventDefault();
+  // Select our target
+  var $target = $($(this).data('target'));
+  // Hide all tabs
+  $('.tab-contents .tab').removeClass('active');
+  // Show only $target tab
+  $target.addClass('active');
+});
